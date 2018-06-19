f11c6f63b297a0ddc17c5326384b74af0e65fcf8
networkteamotowfl <39194423+networkteamotowfl@users.noreply.github.com>
2018-06-19 10:15:13 -0400

    Update config.php

--- config.php
+++ config.php
@@ -12,8 +12,8 @@
  * Copy this file to your working directory, rename it to config.php and update the section below with your UniFi
  * controller details and credentials
  */
-$controlleruser     = 'admin'; // the user name for access to the UniFi Controller
-$controllerpassword = '0t0w1+nc17'; // the password for access to the UniFi Controller
+$controlleruser     = 'username'; // the user name for access to the UniFi Controller
+$controllerpassword = 'password'; // the password for access to the UniFi Controller
 $controllerurl      = 'https://127.0.0.1:8443'; // full url to the UniFi Controller, eg. 'https://22.22.11.11:8443'
 $controllerversion  = '5.6.37'; // the version of the Controller software, eg. '4.6.6' (must be at least 4.0.0)
 /**
